123f990ff2c9e7ab8be36570e814f2fe7f607c7e
Carson Canales and David Cuthell <carson@>
2017-07-06 10:30:11 -0700

    add working submit button to form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-var where = $("input:radio[name=loc]:checked").val();
+$(document).ready(function(){
+  $("#thisForm").submit(function(event){
+    var myName = $("input#name").val();
+    var myAddress = $("input#address").val();
+    var myEmail = $("input#email").val();
+    var myProduct = $("#product").val();
+    var mySize = $("input:radio[name=size]:checked").val();
+    var myDate = $("#date").val();
+    var myColor = $("#color").val();
+
+    $("#aname").text(myName);
+    $("#aaddress").text(myAddress);
+    $("#aemail").text(myEmail);
+    $("#aproduct").text(myProduct);
+    $("#asize").text(mySize);
+    $("#adate").text(myDate);
+    $("#acolor").text(myColor);
+
+    event.preventDefault();
+  });
+});
